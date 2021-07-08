c14752326eb8426534fc2fc42466963a67076a22
周捷 <zhoujie@tuzhanai.com>
2021-07-08 17:14:44 +0800

    Site updated: 2021-07-08 17:14:44

--- js/utils.js
+++ js/utils.js
@@ -26,37 +26,36 @@
 /**
  * @description 判断当前页面是否为活动页
  */
-(function () {
-    // 网页当前状态判断
-    let state, visibilityChange, status, doc = document;
-    if (typeof document.hidden !== 'undefined') {
-        visibilityChange = 'visibilitychange';
-        state = 'visibilityState';
-    } else if (typeof document.mozHidden !== 'undefined') {
-        visibilityChange = 'mozvisibilitychange';
-        state = 'mozVisibilityState';
-    } else if (typeof document.msHidden !== 'undefined') {
-        visibilityChange = 'msvisibilitychange';
-        state = 'msVisibilityState';
-    } else if (typeof document.webkitHidden !== 'undefined') {
-        visibilityChange = 'webkitvisibilitychange';
-        state = 'webkitVisibilityState';
-    }
-    let docText = doc.title;
-    // 添加监听器，在title里显示状态变化
-    doc.addEventListener(visibilityChange, function () {
-        if (doc[state] === 'visible') {
-            doc.title = '欢迎回来！d(`･∀･)b 👏';
-            status = setTimeout(() => {
-                doc.title = docText;
-            }, 1000);
-        } else {
-            doc.title = '藏起来了d(`x_x)b';
-            if (status) {
-                clearTimeout(status);
-            }
-        }
-    }, false);
-    // 初始化页面状态
-    // doc.title = '吕钒的后花园';
-}());
+// (function () {
+//     // 网页当前状态判断
+//     let state, visibilityChange, status, doc = document;
+//     if (typeof document.hidden !== 'undefined') {
+//         visibilityChange = 'visibilitychange';
+//         state = 'visibilityState';
+//     } else if (typeof document.mozHidden !== 'undefined') {
+//         visibilityChange = 'mozvisibilitychange';
+//         state = 'mozVisibilityState';
+//     } else if (typeof document.msHidden !== 'undefined') {
+//         visibilityChange = 'msvisibilitychange';
+//         state = 'msVisibilityState';
+//     } else if (typeof document.webkitHidden !== 'undefined') {
+//         visibilityChange = 'webkitvisibilitychange';
+//         state = 'webkitVisibilityState';
+//     }
+//     let docText = doc.title;
+//     // 添加监听器，在title里显示状态变化
+//     doc.addEventListener(visibilityChange, function () {
+//         if (doc[state] === 'visible') {
+//             doc.title = '欢迎回来！d(`･∀･)b 👏';
+//             status = setTimeout(() => {
+//                 doc.title = docText;
+//             }, 1000);
+//         } else {
+//             doc.title = '藏起来了d(`x_x)b';
+//             if (status) {
+//                 clearTimeout(status);
+//             }
+//         }
+//     }, false);
+//     // 初始化页面状态
+// }());
